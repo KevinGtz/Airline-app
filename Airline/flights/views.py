bfbf8f9342828ac02d5c306d714f9efcde937914
Kevin Gutierrez <kevin@novelistik.com>
2017-03-07 14:40:49 -0600

    Created templates and a new views to show all created flights and to show the detail for a selected flight. Also a HTTP400 exception for not found flight ids.

--- Airline/flights/views.py
+++ Airline/flights/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
+from .models import Flight
 # Create your views here.
 
 
+# Search Flights
 def index(request):
-    return HttpResponse("index")
+    pass
 
 
-def flights(request):
-    return HttpResponse("flights")
+# Show a list of Flights
+def list_flights(request):
+    latest_flights_list = Flight.objects.order_by('?')[:10]
+    context = {
+        'latest_flights_list': latest_flights_list
+    }
+    return render(request, 'flights/flights.html', context=context)
+
+
+# Show Flight Details
+def flight_details(request, flight_id):
+    flight = get_object_or_404(Flight, pk=flight_id)
+    return render(request, 'flights/flight_detail.html', {'flight': flight})
+
+
+# Create Flights -*- (KLG 03/07/17) I think this need to be only in the admin page -*-
+def create_flights(request):
+    pass
