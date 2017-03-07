cf33793542e84502f3b50e6de20f0fd3d9035bed
Kevin Gutierrez <kevin@novelistik.com>
2017-03-06 21:40:39 -0600

    Modified the flight class and implement the admin services.

--- Airline/flights/models.py
+++ Airline/flights/models.py
@@ -4,10 +4,15 @@ from django.db import models
 
 
 class Flight(models.Model):
+    flight_name = models.CharField(max_length=100, default='name of flight')
     flight_number = models.IntegerField(null=False)
-    departure_city = models.CharField(max_length=30)
-    arrival_city = models.CharField(max_length=30)
+    departure_city = models.CharField(max_length=100)
+    arrival_city = models.CharField(max_length=100)
     departure_time = models.TimeField('departure time')
     arrival_time = models.TimeField('arrival time')
     departure_date = models.DateField('departure date')
     arrival_date = models.DateField('arrival date')
+
+    def __str__(self):
+        return self.flight_name
+
